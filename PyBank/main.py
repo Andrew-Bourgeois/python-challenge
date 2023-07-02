93b6737515537118f030fe8a3161668c3e44b6ed
Andrew-Bourgeois <experiment626@macbook-pro.lan>
2023-07-02 13:11:58 -0400

    Update comments

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,7 +1,7 @@
 import csv
 import os
 
-# global variables to track specific details
+# create variables to track specific details
 row_count = 0
 net_pl = 0
 last_pl = 0
@@ -14,31 +14,34 @@ largest_dec_date = ""
 
 #   There are two columns in raw data: "Date" and "Profit/Losses"
 #   create a Python script which:
-#   * opens and pulls in the raw data
+#   opens and pulls in the raw data
+#   create path to raw data
 raw_data_csv = os.path.join("Resources", "budget_data.csv")
 
 with open(raw_data_csv) as raw_data:
     rw_data = csv.reader(raw_data, delimiter=",")
 
+    # store header row
     headers = next(rw_data, None)
 
+    # loop through remaining lines
     for row in rw_data: 
-        #   * totals the number of months in the data set
+        #   increment the total number of months in the data set
         row_count += 1 
-        #   * Net total P&L over the entire period
+        #   Net total P&L over the entire period
         net_pl += float(row[1])
-        #   * The changes of profit and loss from period to period and the average of those changes
+        #   The changes of profit and loss from period to period and the average of those changes
         current_pl = float(row[1]) - last_pl
         # track running total of monthly changes for avg after first row
         if row_count > 1:
             tot_avg += current_pl
         # update last profit and loss
         last_pl = float(row[1])
-        #   * The Greatest increase in profits (date and amount) over entire period
+        #   The Greatest increase in profits (date and amount) over entire period
         if current_pl > largest_inc:
             largest_inc = current_pl
             largest_inc_date = row[0]
-        #   * greatest decrease in profit (date and amount)
+        #   greatest decrease in profit (date and amount)
         elif current_pl < largest_dec:
             largest_dec = current_pl
             largest_dec_date = row[0]
@@ -49,7 +52,7 @@ results = []
 # add print lines to results using .extend() and a tuple of results strings
 results.extend(
     ("Financial Analysis", 
-    "___________________________", 
+    "--------------------------", 
     f"Total Months: {row_count}", 
     f"Total: ${net_pl:.0f}", 
     f"Average Change: ${(tot_avg / (row_count - 1)):.2f}", 
@@ -62,9 +65,10 @@ print()
 for line in results:
     print(line)
 
-# create output file
+# create output file path
 output_file = os.path.join("analysis", "pybank_analysis.txt")
 
+# write results to the new txt ouput file
 with open(output_file, "a") as analysis:
     for line in results:
         analysis.write(line + "\n")
